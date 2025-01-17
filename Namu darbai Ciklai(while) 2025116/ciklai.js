c7d9c2548b59a03307457cd26b63b08c9e7138d3
AugustCode <a.cesnul@gmail.com>
2025-01-17 22:57:23 +0200

    Naktipieciai

--- Namu darbai Ciklai(while) 2025116/ciklai.js
+++ Namu darbai Ciklai(while) 2025116/ciklai.js
@@ -1,34 +1,19 @@
 // Sukurkite funkciją, kuri sukuria atsitiktinių skaičių masyvą iš N elementų. Atsitiktiniai skaičiai nuo M iki O;
 // pvz:
-function rand(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-const skaičiai = [];
-
-index = 1;
-while (index <= 4) {
-    const atsitiktinisSk = rand(100, 105);
-    skaičiai.push(atsitiktinisSk);
-    index++
-}
-console.log(skaičiai);
-// console.log(generuotiAtsitiktinius(100, 105)); 
-// atsakymas konsolėje: [101, 101, 100, 105]
-
-
 // function rand(min, max) {
 // 	return Math.floor(Math.random() * (max - min + 1)) + min;
 // }
+// const skaičiai = [];
 
-// const atsitiktiniai = [];
-// index = 0;
-// while (index < 10) {
-// 	const atsitiktinisSk = rand(1, 100); //44
-// 	atsitiktiniai.push(atsitiktinisSk);
-// 	index++;
+// index = 1;
+// while (index <= 4) {
+//     const atsitiktinisSk = rand(100, 105);
+//     skaičiai.push(atsitiktinisSk);
+//     index++
 // }
-
-// console.log(atsitiktiniai);
+// console.log(skaičiai);
+// console.log(generuotiAtsitiktinius(100, 105)); 
+// atsakymas konsolėje: [101, 101, 100, 105]
 
 
 // Reikia sukurti funkciją, kuriai padavus skaičių masyvą, reiktų atlikti filtraciją ir palikti tik lyginius skaičius masyveir jį atiduoti iš funkcijos.
@@ -36,13 +21,36 @@ console.log(skaičiai);
 // const masyvas = [1, 2, 3, 4];
 
 // function filtruotiNelyginius(masyvas) {
+    
 // let index = 0;
 
 // while(index < masyvas.length)
 // {
-//     if(masyvas[index] % 2 > 0){
-//         masyvas.splice(index-1, 1)
+//     if(masyvas[index] % 2 !== 0){
+//         masyvas.splice(index, 1);
 //     }
 // index++;
 // }
 // }
+
+// filtruotiNelyginius(masyvas);
+// console.log(masyvas);
+
+// Sukurkite funkciją, kuri console.log’e parašo tik skaičius iš masyvo M, mažesnius nei N.
+// pvz: 
+// tikMazesni([4,5,6,7,2,4], 5);
+// atsakymas konsolėje: 4, 2, 4 
+
+const masyvas = [4,5,6,7,2,4];
+
+function tikMazesni(masyvas) {
+    index = 0;
+    while(index < masyvas.length) {
+        if(masyvas[index] <= 5) {
+            masyvas.splice(index,1);
+        }
+        index++;
+    }
+}
+tikMazesni(masyvas);
+console.log(masyvas);
