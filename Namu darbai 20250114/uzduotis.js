3f0078282038b218fac36ba2ac40dcf83b33de2c
AugustCode <a.cesnul@gmail.com>
2025-01-17 11:52:56 +0200

    zinute

---
+++ Namu darbai 20250114/uzduotis.js
@@ -0,0 +1,16 @@
+function tekstoPakitimas(){
+    let tekstas = document.getElementById("inputoTekstas").value; 
+    document.getElementById("my-button").innerHTML = tekstas;
+}  
+
+function fonoSpalva() {
+     
+    let selectedColor = document.getElementById("my-color").value;
+    document.body.style.backgroundColor = selectedColor
+}
+
+
+
+
+
+
